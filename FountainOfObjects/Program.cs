bc9d2e59a7e44254339201175931d4a828a66a8a
Jeffrey Seneff <jeffsnff@gmail.com>
2025-06-05 06:50:08 -0400

    First push for Fountain of Objects

---
+++ FountainOfObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FountainOfObjects
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello Fountain of Objects!");
+    }
+  }
+}
